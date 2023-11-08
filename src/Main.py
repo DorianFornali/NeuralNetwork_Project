83685aa559be7bea1273338521f74a29513d7629
DorianFornali <dorian.fornali@outlook.fr>
2023-11-08 13:24:16 +0100

    initial commit

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,8 @@
+import pandas as pd
+
 if __name__ == '__main__':
-    print("Hello World!")
+    results = pd.read_csv("../databases/results.csv")
+    ranking = pd.read_csv("../databases/fifa_ranking-2023-07-20.csv")
+
+    print(ranking)
+
